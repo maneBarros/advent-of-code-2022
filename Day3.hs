5143928a7297560432f67c56046e3cf1ad25c1ad
Manuel Barros <manuelqmbarros@gmail.com>
2022-12-04 11:47:50 +0000

    Use partial functions in day 3 to make it cleaner

--- Day3.hs
+++ Day3.hs
@@ -8,23 +8,22 @@ type Item = Char
 type Priority = Int
 type Rucksack = [Item]
 
-priority :: Item -> Maybe Priority 
-priority item = lookup item $ zip (['a'..'z'] ++ ['A'..'Z']) [1..]
+priority :: Item -> Priority 
+priority item = fromJust $ lookup item $ zip (['a'..'z'] ++ ['A'..'Z']) [1..]
 
-repeatedItem :: Rucksack -> Maybe Item
+repeatedItem :: Rucksack -> Item
 repeatedItem rucksack = (\(c1,c2) -> sharedItem [c1,c2]) $ splitAt (length rucksack `div` 2) rucksack
 
-sharedItem :: [[Item]] -> Maybe Item
-sharedItem [] = Nothing
-sharedItem (r:rs) = find (\i -> all (i `elem`) rs) r 
+sharedItem :: [[Item]] -> Item
+sharedItem (r:rs) = fromJust $ find (\i -> all (i `elem`) rs) r 
 
 part1 :: [String] -> String
-part1 = show . fmap sum . mapM (priority <=< repeatedItem)
+part1 = show . sum . map (priority . repeatedItem)
 
 -- Problema 2
 groupInThrees :: [a] -> [[a]]
-groupInThrees = map (map snd) . groupBy (\x y -> fst x `div` 3 == fst y `div` 3) . zip [0..]
+groupInThrees = map (map snd) . groupBy (\(i1,_) (i2,_) -> i1 `div` 3 == i2 `div` 3) . zip [0..]
 
 part2 :: [String] -> String
-part2 = show . sum . mapMaybe (priority <=< sharedItem) . groupInThrees
+part2 = show . sum . map (priority . sharedItem) . groupInThrees
 
