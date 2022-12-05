221f11a4e9eb779572b65b31efbfef08505eb98c
Manuel Barros <manuelqmbarros@gmail.com>
2022-12-05 18:35:55 +0000

    Use  on day 1

--- Day1.hs
+++ Day1.hs
@@ -1,15 +1,16 @@
 module Day1 where
 
 import Data.List
+import Data.Function
 
 getElvesCalories :: [String] -> [Int]
 getElvesCalories = 
     map (sum . map read) .
     filter (not . null . head) . 
-    groupBy (\l1 l2 -> null l1 == null l2) 
+    groupBy ((==) `on` null)
 
 part1 :: [String] -> String
 part1 = show . maximum . getElvesCalories
 
 part2 :: [String] -> String
-part2 = show . sum . take 3 . sortOn (\x -> (-x)) . getElvesCalories 
+part2 = show . sum . take 3 . sortOn ((-1)*) . getElvesCalories 
