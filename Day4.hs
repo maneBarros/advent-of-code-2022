ad35d0b53cebb7117290a9d731c83d2f69c66c32
Manuel Barros <manuelqmbarros@gmail.com>
2022-12-05 18:36:14 +0000

    Solve day 4

--- Day4.hs
+++ Day4.hs
@@ -5,10 +5,13 @@ import Data.Bifunctor
 type Assignment = (Int,Int)
 
 contains :: Assignment -> Assignment -> Bool
-(x1,y1) `contains` (x2,y2) = x1 <= x2 && y1 >= y2 
+contains (x1,y1) (x2,y2) = x1 <= x2 && y1 >= y2 
 
 fullOverlap :: Assignment -> Assignment -> Bool
 fullOverlap a b = a `contains` b || b `contains` a
+
+overlap :: Assignment -> Assignment -> Bool
+overlap (x1,y1) (x2,y2) = any (`elem` [x2..y2]) [x1..y1]
 
 readAssignment :: String -> Assignment
 readAssignment = bimap read (read . tail) . break (== '-')
@@ -18,3 +21,6 @@ processLines = map $ bimap readAssignment (readAssignment . tail) . break (== ',
 
 part1 :: [String] -> String
 part1 = show . length . filter (uncurry fullOverlap) . processLines
+
+part2 :: [String] -> String
+part2 = show . length . filter (uncurry overlap) . processLines
--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ solutions = zip [1..] [
     (Day1.part1, Day1.part2),
     (Day2.part1, Day2.part2),
     (Day3.part1, Day3.part2),
-    (Day4.part1, undefined)
+    (Day4.part1, Day4.part2)
     ]
 
 main :: IO ()
